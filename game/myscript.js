4faeeec92025be92165ab19e28777d67025d5ca1
LaiaNO <41057949+LaiaNO@users.noreply.github.com>
2022-05-13 22:01:31 +0200

    mejorar interfaz babylon

--- game/myscript.js
+++ game/myscript.js
@@ -1,17 +1,35 @@
 
-const createScene = function() {
+const createScene = function(engine) {
+
     const scene = new BABYLON.Scene(engine);
-    const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 10, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0));
+
+    // We implement universal camera
+    // https://doc.babylonjs.com/divingDeeper/cameras/camera_introduction 
+    //Parameters: name, alpha, beta, radius, target position, scene
+    var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 0, new BABYLON.Vector3(0, 0, 0), scene);
+    // Positions the camera overwriting alpha, beta, radius
+    camera.setPosition(new BABYLON.Vector3(0, 0, 0));
+    // This attaches the camera to the canvas
     camera.attachControl(canvas, true);
+
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0));
-    const box = BABYLON.MeshBuilder.CreateBox("box", {});
+    
+    // Append glTF model to scene.
+    BABYLON.SceneLoader.Append("/recursos/", "buttons.gltf", scene, function (scene) {
+        // Create a default arc rotate camera and light.
+        scene.createDefaultCameraOrLight(true, true, true);
+
+        // The default camera looks at the back of the asset.
+        // Rotate the camera by 180 degrees to the front of the asset.
+        scene.activeCamera.alpha += Math.PI;
+    });
 
     return scene;
 }
 
-const canvas = document.getElementById("renderCanvas"); // Get the canvas element
+const canvas = document.getElementById("renderCanvasInstructions"); // Get the canvas element
 const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
-const scene = createScene(); //Call the createScene function
+const scene = createScene(engine); //Call the createScene function
 // Register a render loop to repeatedly render the scene
 engine.runRenderLoop(function () {
     scene.render();
