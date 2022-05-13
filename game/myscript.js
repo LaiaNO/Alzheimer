f7c0f1a939c5b024b0daf065e75213fe3e46759b
LaiaNO <41057949+LaiaNO@users.noreply.github.com>
2022-05-13 12:25:02 +0200

    Change model to buttons

--- game/myscript.js
+++ game/myscript.js
@@ -1,7 +1,7 @@
 
 const createScene = function() {
     const scene = new BABYLON.Scene(engine);
-    const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0));
+    const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 10, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0));
     camera.attachControl(canvas, true);
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0));
     const box = BABYLON.MeshBuilder.CreateBox("box", {});
